45c6fb8255d7a3199d3b6962854151766d148e8e
Jmiller1294 <jayman1294@gmail.com>
2021-04-05 23:47:33 -0400

    added styling to Friend component

--- src/components/friends/Friend.js
+++ src/components/friends/Friend.js
@@ -1,20 +1,41 @@
-import React from 'react'
-import { Card } from 'react-bootstrap';
-import friendpic from '../../assets/blank.png';
+import React from 'react';
+import friendpic from '../../assets/profile.jpg';
+import styled from 'styled-components';
+
+export const FriendItem = styled.li`
+    float: left;
+    margin-right: 50px;
+    height: 400px;
+    width: 200px;
+    list-style: none;
+`
+export const FriendCard = styled.figure`
+    height: 300px;
+    width: 200px;
+`
+export const FriendImage = styled.img`
+    height: 250px;
+    width: 200px;
+`
+
+export const FriendInfo = styled.figcaption`
+    height: 50px;
+    width: 200px;
+    font-size: 20px;
+    text-align: center;
+    background-color: white;
+`
 
 const Friend = (props) => {
     const { friend } = props
+    
     return (
-        <div>
-            <Card style={{ width: '18rem' }}>
-            <Card.Img variant="top" src={friendpic} />
-            <Card.Body>
-            <Card.Title>{friend.name}</Card.Title>
-            <Card.Text>  
-            </Card.Text>
-            </Card.Body>
-            </Card>
-        </div>
+        <FriendItem>
+            <FriendCard>
+                <FriendImage src={friendpic} />
+                <FriendInfo>{friend.name}</FriendInfo>
+            </FriendCard>
+        </FriendItem>
     )
 }
 export default Friend;
