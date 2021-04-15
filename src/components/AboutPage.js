2fc8b9a61d9a9661089f0c499d3b92b647ff8c0a
Jmiller1294 <jayman1294@gmail.com>
2021-04-14 20:10:28 -0400

    added text to about page

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -2,7 +2,13 @@ import React from 'react';
 
 const AboutPage = (props) => {
   return(
+    <>
     <h2>About</h2>
+      <p>
+        Social net was created by Justin Miller in 2020 to allow users to make posts, get the latest new, and what their friends are up too.
+        Social net was inspired by social media platforms such as myspace and facebook.
+      </p>
+    </>
   )
 }
 export default AboutPage;
